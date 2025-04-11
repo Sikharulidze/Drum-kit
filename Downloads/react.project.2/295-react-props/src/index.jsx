30398fec79442fd340b32449e5121b4f342f17c1
Mariam Sikharulidze <mariam.sikha.22@gmail.com>
2025-04-11 11:54:53 +0400

    Replace Beyoncé with Eminem contact info and picture

--- Downloads/react.project.2/295-react-props/src/index.jsx
+++ Downloads/react.project.2/295-react-props/src/index.jsx
@@ -6,7 +6,7 @@ function Card(props) {
     <div>
       <h2>{props.name}</h2>
       <img src={props.img} alt="avatar_img" />
-      <p>{props.tell}</p>
+      <p>{props.tel}</p>
       <p>{props.email}</p>
     </div>
   );
@@ -16,21 +16,21 @@ ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
 
-    <card
-      name="Beyonce"
-      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+    <Card
+      name="Eminem"
+      img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRqUv3bFkqjj9bitaId1Mq5JCT9fVCExtMZA&s"
       tel="+123 456 789"
-      email="b@beyonce.com"
+      email="eminem@shadyrecords.com"
     />
 
-    <card
+    <Card
       name="Jack Bauer"
       img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
       tel="+987 654 321"
       email="jack@nowhere.com"
     />
 
-    <card
+    <Card
       name="Chuck Norris"
       img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
       tel="+918 372 574"
@@ -39,9 +39,3 @@ ReactDOM.render(
   </div>,
   document.getElementById("root")
 );
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
